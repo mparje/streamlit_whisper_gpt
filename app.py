abd47d8088244298e182a41f73ae6c553b3af80e
mparje <126131806+mparje@users.noreply.github.com>
2023-04-21 19:29:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,28 @@ from audiorecorder import audiorecorder
 from streamlit_player import st_player
 from audio_recorder_streamlit import audio_recorder
 import whisper
-import torch
-#torch.cuda.empty_cache()
-
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+import io
 
 st.title('Pruebas de captura de voz usando audio recorder y transcripción usando whisper')
 
 st.write("""Haga una grabación""")
 
+# Load the Whisper model
+model = whisper.Transcriber(model_path='base')
 
-
-
-
-model = whisper.load_model("base",'cpu')
-
+# Record audio
 audio_bytes = audio_recorder()
 
-
-
 if audio_bytes:
-    with open("audio.mp3", "wb") as file:
-        file.write(audio_bytes)
+    # Write audio to file
+    with open('audio.mp3', 'wb') as f:
+        f.write(audio_bytes)
 
-    st.audio(audio_bytes, format="audio/wav")
-    result = model.transcribe("audio.mp3",language='Spanish')
-    #result = model.transcribe("audio.mp3",fp16=False, language='Spanish')
-    st.text(result["text"])
+    # Transcribe audio
+    with open('audio.mp3', 'rb') as f:
+        audio_bytes = io.BytesIO(f.read())
+    result = model.transcribe(audio_bytes, language='Spanish')
+
+    # Display audio and transcription
+    st.audio(audio_bytes, format='audio/wav')
+    st.text(result['text'])
